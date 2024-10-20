5464360cfab7e3ce12ab70b45b7c21aa29e6ee34
JulianPipi <Julianpinia@gmail.com>
2024-10-20 18:02:18 -0300

    comiteo 2

--- TP8_GRUPO_8/TP8_GRUPO_8/Vistas/EliminarSucursal.aspx.cs
+++ TP8_GRUPO_8/TP8_GRUPO_8/Vistas/EliminarSucursal.aspx.cs
@@ -20,6 +20,8 @@ namespace Vistas
             NegocioSucursal negSuc = new NegocioSucursal();
             int idSucursal = int.Parse(txtIdSucursal.Text);
            
+            negSuc.eliminarSucursal(idSucursal);
+
             //TERMINAR
         }
     }
